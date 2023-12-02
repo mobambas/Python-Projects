c0a0acc4d9b2fd19f10b76824039f5325ce7e4a8
Shriyansh Singh <99650602+mobambas@users.noreply.github.com>
2023-12-02 16:30:28 +0800

    Update Weight_Converter.py

--- Weight_Converter.py
+++ Weight_Converter.py
@@ -1,35 +1,51 @@
 from tkinter import *
 
+def convert_weight():
+    try:
+        input_weight = float(entry_weight.get())
+
+        # Clear previous results
+        for label in result_labels:
+            label.config(text="")
+
+        # Convert to other units
+        grams = input_weight * 1000
+        pounds = input_weight * 2.20462
+        ounces = input_weight * 35.274
+
+        # Display results
+        result_labels[0].config(text=f"{grams:.2f} grams")
+        result_labels[1].config(text=f"{pounds:.2f} pounds")
+        result_labels[2].config(text=f"{ounces:.2f} ounces")
+
+    except ValueError:
+        for label in result_labels:
+            label.config(text="Invalid input")
+
 root = Tk()
 root.title("Weight Converter")
-root.geometry("500x500")
+root.geometry("500x300")
 
+# Font style
 f1 = ("bold")
 
+# Labels and Entry
+label_instruction = Label(root, text="Please enter your weight:")
+label_instruction.grid(row=0, column=0, columnspan=2, pady=10)
 
-def convert():
-    label_5 = Label(root, text=float(entry_1.get()) * 1000, font=f1)
-    label_5.grid(row=7, column=2)
+entry_weight = Entry(root)
+entry_weight.grid(row=0, column=2, padx=10, pady=10)
 
-    label_6 = Label(root, text=float(entry_1.get()) * 2, font=f1)
-    label_6.grid(row=7, column=3)
+convert_btn = Button(root, text="Convert", command=convert_weight)
+convert_btn.grid(row=0, column=3, padx=10, pady=10)
 
-    label_7 = Label(root, text=float(entry_1.get()) * 35, font=f1)
-    label_7.grid(row=7, column=4)
+# Results Labels
+result_labels = []
+units = ["Grams", "Pounds", "Ounces"]
 
-
-label_1 = Label(root, text="Please enter your desired weight in KGS")
-label_1.grid(row=1, column=2, padx=10)
-entry_1 = Entry(root)
-entry_1.grid(row=1, column=3, padx=10)
-convert_btn = Button(root, text="Convert", command=convert)
-convert_btn.grid(row=1, column=4, padx=10)
-label_2 = Label(root, text="Grams")
-label_2.grid(row=4, column=2)
-label_3 = Label(root, text="Pounds")
-label_3.grid(row=4, column=3)
-label_4 = Label(root, text="Ounces")
-label_4.grid(row=4, column=4)
+for i, unit in enumerate(units):
+    label_result = Label(root, text="", font=f1)
+    label_result.grid(row=3, column=i + 1, pady=5)
+    result_labels.append(label_result)
 
 root.mainloop()
-
