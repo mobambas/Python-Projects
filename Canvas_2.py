d722a621f1ca8a71d30702bef2cbf8971e94dd2b
Shriyansh Singh <99650602+mobambas@users.noreply.github.com>
2023-12-02 16:20:05 +0800

    Update Canvas_2.py

--- Canvas_2.py
+++ Canvas_2.py
@@ -1,52 +1,72 @@
-# from tkinter import *
-#
-#
-# root = Tk()
-# root.title("Image Handling")
-# root.geometry("500x500")
-# canvas = Canvas(root,width=450,height=450, bg="white")
-# canvas.pack()
-# shape = canvas.create_oval(22,22,50,50,fill="yellow")
-# def left(event):
-#     x = -5
-#     y = 0
-#     canvas.move(shape,x,y)
-# def right(event):
-#     x = 5
-#     y = 0
-#     canvas.move(shape,x,y)
-#
-# def down(event):
-#     x = 0
-#     y = 5
-#     canvas.move(shape,x,y)
-#
-# def up(event):
-#     x = 0
-#     y = -5
-#     canvas.move(shape, x, y)
-#
-# root.bind('<Left>', left)
-# root.bind('<Right>', right)
-# root.bind('<Down>', down)
-# root.bind('<Up>', up)
-# def pressed(event):
-#     x,y=0,0
-#     if event.char == 'w':
-#         x = 0
-#         y = -10
-#     if event.char == 'a':
-#         x = -10
-#         y = 0
-#     if event.char == 's':
-#         x = 0
-#         y = 10
-#     if event.char == 'd':
-#         x = 10
-#         y = 0
-#     canvas.move(shape,x,y)
-# root.bind('<Key>', pressed)
-# root.mainloop()
+from tkinter import *
+
+root = Tk()
+root.title("Image Handling")
+root.geometry("500x500")
+canvas = Canvas(root, width=450, height=450, bg="white")
+canvas.pack()
+
+shape = canvas.create_oval(22, 22, 50, 50, fill="yellow")
+
+
+def move_shape(x, y):
+    canvas.move(shape, x, y)
+    check_boundaries()
+
+
+def check_boundaries():
+    x1, y1, x2, y2 = canvas.coords(shape)
+    canvas_width = canvas.winfo_width()
+    canvas_height = canvas.winfo_height()
+
+    if x1 < 0:
+        canvas.move(shape, -x1, 0)
+    elif x2 > canvas_width:
+        canvas.move(shape, canvas_width - x2, 0)
+
+    if y1 < 0:
+        canvas.move(shape, 0, -y1)
+    elif y2 > canvas_height:
+        canvas.move(shape, 0, canvas_height - y2)
+
+
+def left(event):
+    move_shape(-5, 0)
+
+
+def right(event):
+    move_shape(5, 0)
+
+
+def down(event):
+    move_shape(0, 5)
+
+
+def up(event):
+    move_shape(0, -5)
+
+
+def pressed(event):
+    x, y = 0, 0
+    if event.char == 'w':
+        y = -10
+    elif event.char == 'a':
+        x = -10
+    elif event.char == 's':
+        y = 10
+    elif event.char == 'd':
+        x = 10
+
+    move_shape(x, y)
+
+
+root.bind('<Left>', left)
+root.bind('<Right>', right)
+root.bind('<Down>', down)
+root.bind('<Up>', up)
+root.bind('<Key>', pressed)
+
+root.mainloop()
 
 from tkinter import *
 root = Tk()
