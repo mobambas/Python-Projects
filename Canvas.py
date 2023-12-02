cfd8da323ccf15ced022d1f51c7cbbd6cf62f08a
Shriyansh Singh <99650602+mobambas@users.noreply.github.com>
2023-12-02 16:19:02 +0800

    Update Canvas.py

--- Canvas.py
+++ Canvas.py
@@ -4,27 +4,29 @@ root = Tk()
 root.title("Canvas")
 root.geometry("500x500")
 
-my_canvas = Canvas(root, width=400, height=400, bg="white")
-my_canvas.pack(pady=45)
+# Create a frame to hold the canvas and add padding
+frame = Frame(root)
+frame.pack(pady=10)
 
-# my_canvas.create_line(90,60,120,140, fill="orange")
-# my_canvas.create_line(45,30,60,70, fill="magenta")
-# # my_canvas.create_line(5,90,50,90, fill="purple")
-# my_canvas.create_rectangle(24,45,60,98, fill="black")
+# Create a canvas with a colorful border
+canvas_width = 400
+canvas_height = 400
+canvas_border_color = "cyan"
+canvas_border_width = 10
 
-# my_canvas.create_line(100,100,200,100, fill="black")
-# my_canvas.create_rectangle(50,50,100,75, fill="black")
-# my_canvas.create_rectangle(200,50, 250, 75, fill="black")
-# my_canvas.create_rectangle(75,125,225,150, fill="pink")
+canvas = Canvas(frame, width=canvas_width, height=canvas_height, bg="white", highlightbackground=canvas_border_color, highlightthickness=canvas_border_width)
+canvas.pack()
 
-my_canvas.create_oval(20,150,380,250,fill="green")
-my_canvas.create_oval(50,170,70,230, fill="blue")
-my_canvas.create_line(60,232,60,242, fill="black", width=10)
-my_canvas.create_line(157,248,157,253, fill="black", width=10)
-my_canvas.create_line(254,248,254,253, fill="black", width=10)
-my_canvas.create_line(351,229,351,239, fill="black", width=10)
-my_canvas.create_line(110,200,290,200, fill="red", width=5)
-my_canvas.create_line(155,190,155,210, fill="purple", width=2.5)
-my_canvas.create_line(200,190,200,210, fill="purple", width=2.5)
-my_canvas.create_line(245,190,245,210, fill="purple", width=2.5)
+# Draw shapes on the canvas
+canvas.create_oval(20, 150, 380, 250, fill="green")
+canvas.create_oval(50, 170, 70, 230, fill="blue")
+canvas.create_line(60, 232, 60, 242, fill="black", width=10)
+canvas.create_line(157, 248, 157, 253, fill="black", width=10)
+canvas.create_line(254, 248, 254, 253, fill="black", width=10)
+canvas.create_line(351, 229, 351, 239, fill="black", width=10)
+canvas.create_line(110, 200, 290, 200, fill="red", width=5)
+canvas.create_line(155, 190, 155, 210, fill="purple", width=2.5)
+canvas.create_line(200, 190, 200, 210, fill="purple", width=2.5)
+canvas.create_line(245, 190, 245, 210, fill="purple", width=2.5)
+
 root.mainloop()
