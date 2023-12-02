b61f15179689979da052e1862af682fdd8a784cf
Shriyansh Singh <99650602+mobambas@users.noreply.github.com>
2023-12-02 16:23:11 +0800

    Update Listbox.py

--- Listbox.py
+++ Listbox.py
@@ -1,30 +1,37 @@
 from tkinter import *
 
+def delete():
+    selected_index = listbox.curselection()
+    if selected_index:
+        listbox.delete(selected_index)
+        label.config(text="Item deleted")
+
+def select():
+    selected_index = listbox.curselection()
+    if selected_index:
+        selected_item = listbox.get(selected_index)
+        label.config(text=f"Selected Item: {selected_item}")
+
 root = Tk()
 root.title("Listbox")
 root.geometry("500x500")
 
-def delete():
-    listbox.delete(ANCHOR)
-    label.config(text=" ")
-
-label  = Label(root)
-label.pack(pady = 15)
+label = Label(root)
+label.pack(pady=15)
 
 listbox = Listbox(root)
-listbox.pack(pady = 15)
+listbox.pack(pady=15)
 listbox.insert(END, "This is the first item")
 listbox.insert(END, "This is the second item")
 
-list = ["magenta", "orange", "violet", "indigo", "blue", "cyan", "red", "purple", "yellow", "green"]
-for item in list:
+colors = ["magenta", "orange", "violet", "indigo", "blue", "cyan", "red", "purple", "yellow", "green"]
+for item in colors:
     listbox.insert(END, item)
 
+button_1 = Button(root, text="Select", command=select)
+button_1.pack(pady=15)
 
-button_1 = Button(root, text="Select", command = None)
-button_1.pack(pady = 15)
-button_2 = Button(root, text="Delete", command = delete)
-button_2.pack(pady = 15)
+button_2 = Button(root, text="Delete", command=delete)
+button_2.pack(pady=15)
+
 root.mainloop()
-
-
